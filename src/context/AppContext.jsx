925b6ed55aecc8258c829bf2188f06dceaa2a148
Harshit <hktiwari980@gmail.com>
2025-12-13 16:00:04 +0530

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({children}) =>{
+       
+     return (
+        <AppContext.Provider value={value}>
+             {children}
+        </AppContext.Provider>
+     )
+}
+
+export const useAppContext = () => useContext(AppContext)
