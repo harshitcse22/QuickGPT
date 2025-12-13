d66e22e0a789a800467a9be48db3b20f0dc7e270
Harshit <hktiwari980@gmail.com>
2025-12-13 20:05:23 +0530

    credit purchase option added in sidebar and dark mode toggle also

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import React from 'react'
 import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import moment from 'moment'
 
 const Sidebar = () => {
 
-   const {chats, setSelecttedChat, theme, setTheme, user} = useAppContext()
+   const {chats, setSelecttedChat, theme, setTheme, user, navigate} = useAppContext()
    const [search, setSearch] = useState('')
       
 
@@ -47,7 +48,7 @@ const Sidebar = () => {
                         <p className='truncate w-full'>
                           {chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name}
                         </p>
-                        <p className='text-xs text-gray-500 dark:text-[#B1A6C0]'>{chat.updatedAt}</p>
+                        <p className='text-xs text-gray-500 dark:text-[#B1A6C0]'>{moment(chat.updatedAt).fromNow()}</p>
                     </div>
                     <img src={assets.bin_icon} className='hidden group-hover:block
                      w-4 cursor-pointer not-dark:invert' alt="" />
@@ -55,6 +56,47 @@ const Sidebar = () => {
               ))
             }
         </div>
+
+        {/** community images */}
+        <div  onClick={()=> {navigate('/')}} className='flex items-center gap-2 p-3 mt-4 border
+          border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103
+          transition-all'>
+            <img src={assets.gallery_icon} className='w-4.5 not-dark:invert' alt="" />
+            <div className='flex flex-col text-sm'>
+               <p>Community Images</p>
+            </div>
+        </div>
+
+
+        {/** Credit Purchase Options*/}
+        <div  onClick={()=> {navigate('/credits')}} className='flex items-center gap-2 p-3 mt-4 border
+          border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103
+          transition-all'>
+            <img src={assets.diamond_icon} className='w-4.5 dark:invert' alt="" />
+            <div className='flex flex-col text-sm'>
+               <p>Credits : {user?.credits}</p>
+               <p className='text-xs text-gray-400'>Purchase credits to use quickgpt</p>
+            </div>
+        </div>
+
+        {/** Dark Mode Toggle */}
+          <div className='flex items-center justify-between gap-2 p-3 mt-4 border
+          border-gray-300 dark:border-white/15 rounded-md'>
+             <div className='flex items-center gap-2 text-sm'>
+                 <img src={assets.theme_icon} className='w-4 not-dark:invert' alt=""/>
+                 <p>Dark Mode</p>
+             </div>
+             <label className='relative inline-flex cursor-pointer'>
+                <input onChange={()=> setTheme(theme === 'dark' ? 'light' : 'dark')} 
+                type="checkbox" className='sr-only peer' checked={theme === 'dark'}/>
+                <div className='w-9 h-5 bg-gray-400 rounded-full
+                  peer-checked:bg-purple-600 transition-all'>
+                </div>
+                <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full
+                 transition-transform peer-checked:translate-x-4'></span>
+             </label>
+        </div>
+         
     </div>
   )
 }
