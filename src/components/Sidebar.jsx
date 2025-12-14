9c92aee24f10818289cc539e423ba2338a46fb23
Harshit <hktiwari980@gmail.com>
2025-12-14 20:50:19 +0530

    toggle mode fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,9 +11,7 @@ const Sidebar = () => {
       
 
   return (
-    <div className='flex flex-col h-screen min-w-72 p-5 drak:bg-gradient-to-b from-
-  [#242124]/30 to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl
-    transition-all duration-500 max-md:absolute left-0 z-1'>
+    <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b dark:from-[#242124]/30 dark:to-[#000000]/30 border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1'>
       {/* Logo */}
       <img src={theme === 'dark' ? assets.logo_full : assets.logo_full_dark} alt="" 
         className='w-full max-w-48'/>
